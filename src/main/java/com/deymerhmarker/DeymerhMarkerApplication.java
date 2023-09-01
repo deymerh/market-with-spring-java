e4626d3e19df8224c6475a52426ea34868f95b6d
Deymer Hernandez <dhernant@contratista.epm.co>
2023-08-31 21:16:00 -0500

    Initial project

---
+++ src/main/java/com/deymerhmarker/DeymerhMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.deymerhmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeymerhMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeymerhMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deymerhmarker/DeymerhMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deymerhmarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeymerhMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
